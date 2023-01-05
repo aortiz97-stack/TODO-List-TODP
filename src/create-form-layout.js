a9e999d1f8b6a3bae70fe29dda902373d351e55e
Armando <armando.ortiz.jr.19@outlook.com>
2023-01-05 16:28:00 -0600

    Add default toDo tab content in the form box

--- src/create-form-layout.js
+++ src/create-form-layout.js
@@ -16,7 +16,7 @@ const formContents = (formHeader, formSideBar, formMainContent) => {
   }
   function populateFormSideBar() {
     const ul = document.createElement('ul');
-    const sideBarNames = ['To-Do', 'Project', 'Note'];
+    const sideBarNames = ['// To-Do', 'Project', 'Note'];
 
     for (let i = 0; i < sideBarNames.length; i += 1) {
       const li = document.createElement('li');
@@ -31,9 +31,67 @@ const formContents = (formHeader, formSideBar, formMainContent) => {
     return formSideBar;
   }
   function populateFormMainContent() {
-    const div = document.createElement('div');
-    div.classList.add('content-container');
-    formMainContent.appendChild(div);
+    const contentContainer = document.createElement('div');
+    contentContainer.classList.add('content-container');
+    const form = document.createElement('form');
+
+    const htmlList = [];
+    function createNotRadioInputHTML(nameOfLabel, typeOfInput) {
+      const input = document.createElement('input');
+      const label = document.createElement('label');
+      label.for = nameOfLabel;
+      input.name = nameOfLabel;
+      input.id = nameOfLabel;
+      input.type = typeOfInput;
+      label.innerHTML = nameOfLabel.charAt(0).toUpperCase() + nameOfLabel.slice(1);
+      label.appendChild(input);
+
+      htmlList.push(label);
+    }
+    function createRadioInputHTML(radioInputValues, radioInputName) {
+      for (let i = 0; i < radioInputValues.length; i += 1) {
+        const labelInputDiv = document.createElement('div');
+
+        const label = document.createElement('label');
+        label.for = radioInputValues[i];
+        label.innerHTML = (radioInputValues[i])[0].toUpperCase() + (radioInputValues[i]).slice(1);
+        const radioInput = document.createElement('input');
+        radioInput.id = radioInputValues[i];
+        radioInput.value = radioInputValues[i];
+        radioInput.name = radioInputName;
+        radioInput.type = 'radio';
+
+        labelInputDiv.appendChild(radioInput);
+        labelInputDiv.appendChild(label);
+        htmlList.push(labelInputDiv);
+        console.log(`htmlList: ${htmlList}`);
+      }
+    }
+
+    createNotRadioInputHTML('title', 'text');
+    createNotRadioInputHTML('description', 'textarea');
+    createNotRadioInputHTML('due date', 'date');
+
+    const priorityLabel = document.createElement('div');
+    priorityLabel.innerHTML = 'Priority';
+    htmlList.push(priorityLabel);
+
+    createRadioInputHTML(['low', 'medium', 'high'], 'priority');
+
+    const statusLabel = document.createElement('div');
+    statusLabel.innerHTML = 'Status';
+    htmlList.push(statusLabel);
+
+    createRadioInputHTML(['completed'], 'completed');
+    createNotRadioInputHTML('project', 'text');
+
+    for (let i = 0; i < htmlList.length; i += 1) {
+      form.appendChild(htmlList[i]);
+    }
+
+    contentContainer.appendChild(form);
+    formMainContent.appendChild(contentContainer);
+
     return formMainContent;
   }
 
--- src/index.js
+++ src/index.js
@@ -2,14 +2,6 @@ import createToDo from './todo';
 import createProject from './project';
 import createFormLayout from './create-form-layout';
 
-const testToDo = (createToDo())('Eat', 'Eat a burger', '1/4/23', 'ASAP', false);
-const project = (createProject())('Daily Activities');
-console.log(project.toDoList[0]);
-project.addToDoList(testToDo);
-console.log(project.toDoList[0]);
-project.removeToDoList(testToDo);
-console.log(project.toDoList[0]);
-
 const addButton = document.querySelector('div.side-bar button');
 addButton.addEventListener('click', () => {
   const body = document.querySelector('body');
