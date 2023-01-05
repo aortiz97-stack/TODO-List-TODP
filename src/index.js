97e7499e5660df14608ecf424f2a9b7db11ecee5
Armando <armando.ortiz.jr.19@outlook.com>
2023-01-05 13:25:32 -0600

    Populate pop-up form with contents, styling still needed

--- src/index.js
+++ src/index.js
@@ -9,6 +9,47 @@ console.log(project.toDoList[0]);
 project.removeToDoList(testToDo);
 console.log(project.toDoList[0]);
 
+const formContents = (formHeader, formSideBar, formMainContent) => {
+  function populateFormHeader() {
+    const formTitle = document.createElement('h1');
+    formTitle.innerHTML = 'Create a new...';
+    formHeader.appendChild(formTitle);
+
+    const formExitButton = document.createElement('button');
+    formExitButton.innerHTML = 'x';
+    formHeader.appendChild(formExitButton);
+    return formHeader;
+  }
+  function populateFormSideBar() {
+    const ul = document.createElement('ul');
+    const sideBarNames = ['To-Do', 'Project', 'Note'];
+
+    for (let i = 0; i < sideBarNames.length; i += 1) {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      a.innerHTML = sideBarNames[i];
+      a.href = '#';
+      li.appendChild(a);
+      ul.appendChild(li);
+    }
+
+    formSideBar.appendChild(ul);
+    return formSideBar;
+  }
+  function populateFormMainContent() {
+    const div = document.createElement('div');
+    div.classList.add('content-container');
+    formMainContent.appendChild(div);
+    return formMainContent;
+  }
+
+  const populatedFormHeader = populateFormHeader();
+  const populatedFormSideBar = populateFormSideBar();
+  const populatedFormMainContent = populateFormMainContent();
+
+  return { populatedFormHeader, populatedFormSideBar, populatedFormMainContent };
+};
+
 function createBasicGrid(formBox) {
   const formHeader = document.createElement('div');
   formHeader.classList.add('header');
@@ -17,9 +58,11 @@ function createBasicGrid(formBox) {
   const formMainContent = document.createElement('div');
   formMainContent.classList.add('main-content');
 
-  formBox.appendChild(formHeader);
-  formBox.appendChild(formSideBar);
-  formBox.appendChild(formMainContent);
+  const content = formContents(formHeader, formSideBar, formMainContent);
+
+  formBox.appendChild(content.populatedFormHeader);
+  formBox.appendChild(content.populatedFormSideBar);
+  formBox.appendChild(content.populatedFormMainContent);
 
   return formBox;
 }
